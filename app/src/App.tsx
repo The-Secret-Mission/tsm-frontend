811fb6d221d35adf212c6b573cdc151fea9f8d66
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-10 01:37:06 +0900

    :sparkles: Feat: Setting react-router
    라우팅을 위한 디폴트 경로 설정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
