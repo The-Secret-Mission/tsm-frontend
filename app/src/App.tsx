ca549f5492f04e3659fa925c9d85df6beea3cd81
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-10 01:39:20 +0900

    :sparkles: Feat: Set NotFound Page on undefined path
    정의되지 않은 경로에 NotFound 표시

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './Page/NotFound';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/"></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
