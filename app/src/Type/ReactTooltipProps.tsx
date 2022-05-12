f0c604b48338368c802c42570436e371106dc3cf
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-12 22:16:51 +0900

    :art: Update: Define props for custom component

---
+++ app/src/Type/ReactTooltipProps.tsx
@@ -0,0 +1,12 @@
+export type ReactTooltipProps = {
+  'data-for'?: string;
+  'data-tip'?: string;
+  'data-type'?: string;
+  'data-effect'?: string;
+  'data-delay-hide'?: number;
+  'data-delay-show'?: number;
+  'data-text-color'?: string;
+  'data-background-color'?: string;
+  'data-border-color'?: string;
+  'data-tip-disable'?: boolean;
+};
