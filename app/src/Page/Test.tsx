8cfb122ba326f5e3c7ffa5b050367470050d77ba
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-13 16:56:30 +0900

    :sparkles: Feat: Add index page

--- app/src/Page/Test.tsx
+++ app/src/Page/Test.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-// import SignupModule from '../Module/SignupModule';
-// import './Signup.css';
-
+import { Stack } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Button from '../Component/Button';
+{
+  /* <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/temppwd" element={<TempPassword />}></Route>
+          <Route path="test" element={<Test />}></Route>
+          <Route path="/main" element={<MainGate />}></Route>
+          <Route path="/me" element={<MyInfo />}></Route>
+          <Route path="/new_agency" element={<CreateAgency />}></Route> */
+}
 function Test() {
-  // const [open, setOpen] = useState(false);
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)',
-  });
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
-  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
-  const isLandscape = useMediaQuery({ query: '(orientation: landscape)' });
-  if (isDesktopOrLaptop && isLandscape) {
-    return <div>hello Laptop - LD</div>;
-  } else if (isDesktopOrLaptop && isPortrait) {
-    return <div>hello Laptop - PT</div>;
-  } else if (isTabletOrMobile && isLandscape) {
-    return <div>hello Mobile - LD</div>;
-  } else if (isTabletOrMobile && isPortrait) {
-    return <div>HI</div>;
-  }
-  return <div>What are you?</div>;
+  return (
+    <div>
+      <Stack style={{ padding: '2rem' }} gap={3}>
+        <Link to="/temp">
+          {' '}
+          <Button kind="fill" value="로그인"></Button>{' '}
+        </Link>
+        <Link to="/signup">
+          <Button kind="fill" value="회원가입"></Button>
+        </Link>
+        <Link to="/temppwd">
+          <Button kind="fill" value="임시비밀번호발급"></Button>
+        </Link>
+        <Link to="/main">
+          <Button kind="fill" value="게이트페이지"></Button>
+        </Link>
+        <Link to="/me">
+          <Button kind="fill" value="내정보관리"></Button>
+        </Link>
+        <Link to="/new_agency">
+          <Button kind="fill" value="새조직창설"></Button>
+        </Link>
+      </Stack>
+    </div>
+  );
 }
 
 export default Test;
