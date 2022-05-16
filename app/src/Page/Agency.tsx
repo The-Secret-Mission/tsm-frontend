23817590ac80664f8f863daa5d2e51418b0aaa4e
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-16 22:36:58 +0900

    :sparkles: Update: Add condition of rendering  mission module
    /agency/10 render dday page
    /agency/11 render over dday page

--- app/src/Page/Agency.tsx
+++ app/src/Page/Agency.tsx
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AgencyAfterModule from '../Module/AgencyAfterModule';
 import AgencyBeforeModule from '../Module/AgencyBeforeModule';
+import MissionModule from '../Module/MissionModule';
 
 import './Agency.css';
 
 function Agency() {
   const params = useParams();
   const [dday, setDday] = useState(false);
+  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     if (params.groupid == '1') setDday(true);
     else if (params.groupid == '2') setDday(true);
+    else if (params.groupid == '10') setDday(true);
+    else if (params.groupid == '11') setDday(true);
+    if (params.groupid == '10') setOpen(true);
+    else if (params.groupid == '11') setOpen(true);
   }, []);
+
   if (!dday) return <AgencyBeforeModule></AgencyBeforeModule>;
-  return <AgencyAfterModule></AgencyAfterModule>;
+  else if (!open) return <AgencyAfterModule></AgencyAfterModule>;
+  return <MissionModule></MissionModule>;
 }
 
 export default Agency;
