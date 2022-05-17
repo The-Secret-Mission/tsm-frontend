2023cb27edd1413efdc6baa67593fa2eb22731bc
jieun kwon <jieunkwon@jieunui-MacBookPro.local>
2022-05-17 17:45:53 +0900

    :sparkles: Update: Add change handler on passwordbox component

--- app/src/Component/PasswordBox.tsx
+++ app/src/Component/PasswordBox.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
+import { ChangeEventHandler } from '../Type/EventHandlerProps';
 import './CSS/PasswordBox.css';
 
-type PasswordBoxProps = {
+type PasswordBoxProps = ChangeEventHandler & {
   placeholder: string;
   style?: React.CSSProperties;
 };
 
 function PasswordBox(props: PasswordBoxProps) {
-  return (
-    <input
-      className="myPasswordBox"
-      type="password"
-      style={props.style}
-      placeholder={props.placeholder}
-    ></input>
-  );
+  return <input className="myPasswordBox" type="password" {...props}></input>;
 }
 
 export default PasswordBox;
